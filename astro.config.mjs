bdced60b5d1dadcc64d31ba9b5d0014ccef581e3
Luis Anberto <newprogrammer02@gmail.com>
2023-08-31 17:00:54 -0600

    adapdando para vercek

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/functions";
+// import netlify from "@astrojs/netlify/functions";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: "server",
-  adapter: netlify()
-});0
+  adapter: vercel()
+});
+0;
