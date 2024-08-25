fb56acaac02ad6a9dca8f8a646b04aa047a2e476
Luis Anberto <newprogrammer02@gmail.com>
2024-08-25 14:01:49 -0600

    Agregando colores en los iconos, y solucionando proflemas con la api

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,7 +1,6 @@
 
-import { type QueryDatabaseParameters, type QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
+import { type PageObjectResponse, type QueryDatabaseParameters, type QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints";
 import { Client } from "@notionhq/client";
-import type { Page, Project } from "../types/types";
 
 
 const DATABASE_ID = "ed2299e9bb5044b3959b42c705324292"
@@ -14,8 +13,8 @@ export const getProjects = async () => {
   const query: QueryDatabaseParameters = {
     database_id: DATABASE_ID,
     filter: {
-      property: 'Estado',
-      select: {
+      property: 'status',
+      status: {
         equals: 'Done'
       }
     },
@@ -29,24 +28,22 @@ export const getProjects = async () => {
 
   try {
     const { results }: QueryDatabaseResponse = await notion.databases.query(query)
-    const projects: Page[] = results as Page[]
 
-    const data: Project[] = projects.map((result) => {
-      const { properties, cover } = result
-      const { Descripcion, Lenguajes, Nombre, Github, Preview } = properties
+    const data = results.map((page: any) => {
+      const { properties, cover } = page
+      const { description, languages, name, github, preview } = properties
 
       return {
-        description: Descripcion ? Descripcion.rich_text.map(e => e.text.content).join('') : '',
-        languages: Lenguajes.multi_select,
-        name: Nombre.title.map(e => (e.text.content))[0],
+        description: description ? description.rich_text.map((e: any) => e.text.content).join('') : '',
+        languages: languages.multi_select,
+        name: name.title.map((e: any) => (e.text.content))[0],
         image: cover.file.url,
-        github: Github.url,
-        preview: Preview.url
+        github: github.url,
+        preview: preview.url
       }
     })
     return data
   } catch (error: any) {
     console.log(error.message)
   }
-
 }
---
+++ src/data/data.ts
@@ -0,0 +1,57 @@
+import Ionic from "../icons/Ionic.astro";
+import Angular from "../icons/Angular.astro";
+import Laravel from "../icons/Laravel.astro";
+import Mysql from "../icons/Mysql.astro";
+import NestJS from "../icons/NestJS.astro";
+import NodeJS from "../icons/NodeJS.astro";
+import React from "../icons/React.astro";
+import Tailwind from "../icons/Tailwind.astro";
+import Typescript from "../icons/Typescript.astro";
+
+export const TAGS = {
+  NEST: {
+    name: "NestJS",
+    class: "bg-gradient-to-r from-red-500 via-red-600 to-red-700 text-white",
+    icon: NestJS,
+  },
+  NODEJS: {
+    name: "Node.js",
+    class: "bg-gradient-to-r from-green-500 via-green-600 to-green-700 text-white",
+    icon: NodeJS,
+  },
+  TYPESCRIPT: {
+    name: "TypeScript",
+    class: "bg-gradient-to-r from-blue-400 via-blue-500 to-blue-600 text-white",
+    icon: Typescript,
+  },
+  REACT: {
+    name: "React",
+    class: "bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 text-white",
+    icon: React,
+  },
+  ANGULAR: {
+    name: "Angular",
+    class: "bg-gradient-to-r from-red-600 via-red-700 to-red-800 text-white",
+    icon: Angular,
+  },
+  LARAVEL: {
+    name: "Laravel",
+    class: "bg-gradient-to-r from-red-700 via-red-800 to-red-900 text-white",
+    icon: Laravel,
+  },
+  TAILWIND: {
+    name: "Tailwind CSS",
+    class: "bg-gradient-to-r from-[#003159] via-[#004080] to-[#0050a0] text-white",
+    icon: Tailwind,
+  },
+  MYSQL: {
+    name: "MySQL",
+    class: "bg-gradient-to-r from-cyan-500 via-cyan-600 to-cyan-700 text-white",
+    icon: Mysql,
+  },
+  IONIC: {
+    name: "Ionic",
+    class: "bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 text-white",
+    icon: Ionic,
+  },
+};
