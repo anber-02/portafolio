839af455292b1553c98e8e94d080741f0509423c
anber-02 <93939343+anber-02@users.noreply.github.com>
2025-06-03 15:06:44 -0600

    fix: email link and add education section and redesigned ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 
 import vercel from "@astrojs/vercel";
 import tailwindcss from "@tailwindcss/vite";
-
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      noExternal: ["@fontsource-variable/onest"],
+    },
   },
   // integrations: [tailwind()],
   output: "server",
--- src/data/data.ts
+++ src/data/data.ts
@@ -7,6 +7,9 @@ import NodeJS from "../icons/NodeJS.astro";
 import React from "../icons/React.astro";
 import Tailwind from "../icons/Tailwind.astro";
 import Typescript from "../icons/Typescript.astro";
+import Django from "../icons/Django.astro";
+import Sql from "../icons/Sql.astro";
+import NextJS from "../icons/Nextjs.astro";
 
 export const TAGS = {
   NEST: {
@@ -16,7 +19,8 @@ export const TAGS = {
   },
   NODEJS: {
     name: "Node.js",
-    class: "bg-gradient-to-r from-green-500 via-green-600 to-green-700 text-white",
+    class:
+      "bg-gradient-to-r from-green-500 via-green-600 to-green-700 text-white",
     icon: NodeJS,
   },
   TYPESCRIPT: {
@@ -41,7 +45,8 @@ export const TAGS = {
   },
   TAILWIND: {
     name: "Tailwind CSS",
-    class: "bg-gradient-to-r from-[#003159] via-[#004080] to-[#0050a0] text-white",
+    class:
+      "bg-gradient-to-r from-[#003159] via-[#004080] to-[#0050a0] text-white",
     icon: Tailwind,
   },
   MYSQL: {
@@ -53,5 +58,22 @@ export const TAGS = {
     name: "Ionic",
     class: "bg-gradient-to-r from-blue-200 via-blue-300 to-blue-400 text-white",
     icon: Ionic,
+  },
+  NEXTJS: {
+    name: "Next.js",
+    class: "bg-gradient-to-r from-gray-800 via-gray-900 to-black text-white",
+    icon: NextJS,
+  },
+  DJANGO: {
+    name: "Django",
+    class:
+      "bg-gradient-to-r from-emerald-700 via-emerald-800 to-emerald-900 text-white",
+    icon: Django,
+  },
+  SQL: {
+    name: "SQL",
+    class:
+      "bg-gradient-to-r from-indigo-500 via-indigo-600 to-indigo-700 text-white",
+    icon: Sql,
   },
 };
